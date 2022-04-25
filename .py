a29e885ff5e2fa2d62b2d3bd383da26476e1d2b2
rehemae <rehemaephraimnanda@gmail.com>
2022-04-25 15:33:45 +0300

    fundamentals

---
+++ .py
@@ -0,0 +1,48 @@
+
+#print statement
+print("hello world")
+print(444)
+print(333)
+print("Rehema Nanda")
+print("Rehema", "Sara", 123 ,"Amina")
+print("Rehema", end="")
+print("Ephraim")
+print("Hello" , end="")
+print("word") 
+#numbers
+num1=(2345)
+print(num1)
+num2=(1234)
+print(num2)
+a=(123)
+b=(123)
+c=a+b
+print(c)
+d=a*b
+print(d)
+e=a/b
+print(e)
+
+#booleans
+
+print(True)
+
+f_bool=False
+print(f_bool)
+
+print(True)
+f_eat=False
+print(f_eat)
+
+print(False)
+water=True
+print(water)
+
+#Strings
+name=("Game of throne")
+print(name)
+
+sentence=("my name is anna")
+sentence1=("my fathers name called Ephraim")
+print(sentence)
+print(sentence1)
